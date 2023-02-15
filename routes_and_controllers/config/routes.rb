755148b26f8ac807f6ad47416ee02aecdf904be3
wtdavis <wtdavis@wesleyan.edu>
2023-02-15 15:14:14 -0500

    show, create

---
+++ routes_and_controllers/config/routes.rb
@@ -0,0 +1,9 @@
+Rails.application.routes.draw do
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # resources :users
+  get 'users/:id', to: 'users#show', as: 'user'
+  post 'users/', to: 'users#create', as: 'users'
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end
