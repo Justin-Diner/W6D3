c853ca18c064a2aa53683afa23fbb95f635c45b5
Justin <jcdiner@gmail.com>
2023-02-16 11:00:28 -0500

    create complete

--- routes_and_controllers/app/controllers/users_controller.rb
+++ routes_and_controllers/app/controllers/users_controller.rb
@@ -5,13 +5,20 @@ class UsersController < ApplicationController
     end
 
 	def create
-		@user = User.new(params['name'], params['email'])
-        @user.save
+		@user = User.new(params.require(:user).permit(:name, :email))
+		#render json: params
+		if @user.save
+			render plain: "Successful!"
+		else 
+			render json: user.errors.full_messages, status: :unprocessable_entity
+		end
 	end
 
 	def show
 		render json: params
 	end
+
+
 
 
 end
